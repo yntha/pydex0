ffd0147cb91ef9cc5f9fa1f5163c0e78d21240be
yntha <126660548+yntha@users.noreply.github.com>
2024-07-24 13:17:17 -0400

    Add the ability to dump dalvik models as JSON

--- pydex/dalvik/models/__init__.py
+++ pydex/dalvik/models/__init__.py
@@ -1,3 +1,8 @@
+import json
+
+from dataclasses import asdict
+from typing import Any
+
 from pydex.dalvik.models.dalvik import (
     DalvikRawItem,
     DalvikHeader,
@@ -13,3 +18,33 @@ from pydex.dalvik.models.dalvik import (
     DalvikProtoIDItem,
     DalvikTypeListItem,
 )
+
+
+class ModelEncoder(json.JSONEncoder):
+    """Custom JSON encoder for encoding models.
+
+    This class extends the ``json.JSONEncoder`` class and provides custom encoding logic for handling ``bytes`` objects.
+    It converts ```bytes``` objects to hexadecimal strings before encoding them to JSON.
+    """
+
+    def default(self, o: Any) -> Any:
+        if isinstance(o, bytes):
+            return o.hex()
+
+        return super().default(o)
+
+    def encode(self, o: Any) -> str:
+        if isinstance(o, bytes):
+            o = o.hex()
+
+        return super().encode(o)
+
+
+def dump_model_json(model: Any):
+    """Dump a model to a JSON string.
+
+    Args:
+        Any model: The model to dump to a JSON string. Must be a dataclass.
+    """
+
+    return json.dumps(asdict(model), indent=2, cls=ModelEncoder)
