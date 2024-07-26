62bfc3175b8a6df03b0e248bf17b73927c251908
yntha <126660548+yntha@users.noreply.github.com>
2024-07-25 22:13:48 -0400

    Update run_livereload.py

--- run_livereload.py
+++ run_livereload.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     server = Server()
     server.watch('source/*.rst', shell('make html'), delay=1)
     server.watch('source/*.py', shell('make html'), delay=1)
-    server.watch('pydex/**/*.py', shell('make html'), delay=1)
     server.watch('source/_static/*', shell('make html'), delay=1)
     server.watch('source/_templates/*', shell('make html'), delay=1)
+    server.watch("source/pipinstall-event", shell("make html"), delay=1)
     server.serve(root='build/html')
